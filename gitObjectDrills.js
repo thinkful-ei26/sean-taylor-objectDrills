726f1b10e3f7e81da96ce407b2c656ed461b56d9
seankrummel <sean.krummel@gmail.com>
2018-10-31 14:02:35 -0500

    completed drill 2

--- gitObjectDrills.js
+++ gitObjectDrills.js
@@ -1,9 +1,23 @@
+'use strict';
+
 const loaf = {
     flour: 300, 
     water:210,
     hydration: function(){
         return (this.flour + this.water)/100;
     }
-}
+};
 console.log(loaf.flour, loaf.water); 
 console.log(loaf.hydration()); 
+
+const obj = {
+    foo: 'Foo',
+    bar: 'Bar',
+    fum: 'Fum',
+    quux: 'Quux',
+    spam: 'Spam',
+};
+
+for (const property in obj) {
+    console.log(`${property}: ${obj[property]}`);
+}
