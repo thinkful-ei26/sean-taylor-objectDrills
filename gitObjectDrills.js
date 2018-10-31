2dfb9034d0f6da65499066bf1e378145b343d8d2
taylormerrill <taylormerrill92@gmail.com>
2018-10-31 12:53:08 -0600

    first github object drill complete

---
+++ gitObjectDrills.js
@@ -0,0 +1,9 @@
+const loaf = {
+    flour: 300, 
+    water:210,
+    hydration: function(){
+        return (this.flour + this.water)/100;
+    }
+}
+console.log(loaf.flour, loaf.water); 
+console.log(loaf.hydration()); 
